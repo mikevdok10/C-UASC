76505e33827b20fc9bd20cba6029d3faae8223c5
mikevdok10 <mikevdok10@gmail.com>
2026-03-27 14:08:33 -0700

    fixed colors

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
-from ultralytics import YOLO
 
 from picamera2 import Picamera2 
+from picamera2.encoders import MJPEGEncoder
+import time 
+
 
 import cv2 
 
 camera = Picamera2()
+
+config = camera.create_preview_configuration()
+camera.configure(config)
 camera.start()
+
+
 
 while True:
     frame = camera.capture_array()
+
+    frame = frame[:, :, [2,1,0]]
     cv2.imshow("Camera", frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
