50aa8076ce6118477de8f74580ffadf484992160
mikevdok10 <mikevdok10@gmail.com>
2026-03-27 13:20:08 -0700

    updating to camera code

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from ultralytics import YOLO
 
-# Load a pretrained YOLOv8 model
-model = YOLO('yolov8n.pt')
-#ayooo ggs
-# ohhhhh shitg lets go
-print("yo gurt")
-print("gurt: yo")
+from picamera2 import Picamera2 
+
+import cv2 
+
+camera = Picamera2()
+camera.start()
+
+while True:
+    frame = camera.capture_array()
+    cv2.imshow("Camera", frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cv2.destroyAllWindows() 
